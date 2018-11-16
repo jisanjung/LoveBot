0c49aa938bc8ae71df995347f39b7a0bc45be580
jisanjung <43708042+jisanjung@users.noreply.github.com>
2018-11-16 17:19:52 -0500

    Updated botDriver

--- botDriver.java
+++ botDriver.java
@@ -1,10 +1,8 @@
 public class botDriver {
 
 	public static void main(String[] args) {
-		
-		Lovebot lb = new Lovebot();
-	
-		lb.botLogic();
-	}
+		botLogic b = new botLogic();
 
+		b.logic();
+	} 
 }
